1bef6504c302b2e252dc6e8a72c2c14b02a388c9
petesusmac <agencylink711@gmail.com>
2024-07-05 09:33:59 +0200

    Merge remote-tracking branch 'origin/master'

--- 4 RANDOMISATION & PYTHON LISTS/4_43 Random Module.py
+++ 4 RANDOMISATION & PYTHON LISTS/4_43 Random Module.py
@@ -1,3 +1,17 @@
 import random
 
+# outputting a normal INTEGER as a random number
 random_integer = random.randint(1, 10)
+print(random_integer)
+
+# outputting a FLOAT generated randomly
+random_float = random.random()
+print(random_float)
+
+# to generate random floating numbers:
+random_float_int = random.random() * 45
+print(random_float_int)
+
+
+
+
