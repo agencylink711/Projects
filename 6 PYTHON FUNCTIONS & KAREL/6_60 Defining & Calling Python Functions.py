acf925dd85c5b586fe04d27f4430f58f6a673335
petesusmac <agencylink711@gmail.com>
2024-07-07 07:29:02 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ 6 PYTHON FUNCTIONS & KAREL/6_60 Defining & Calling Python Functions.py
@@ -0,0 +1,29 @@
+
+print("Hello")
+
+num_char = len("Hello")
+print(num_char)
+
+# Creating our own FUNCTION
+
+# def: because we are creating or defining our function, after the def Keyword,
+# we can give our function a name
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+
+def my_2nd_function():
+    if sky == "clear":
+        print("great day, everyone is out")
+    elif sky == "cloudy":
+        print("mostly staying at home, partly going out & keeping an eye on sky")
+    print("Hello, nice to see you")
+
+print("Let's check how the weather is today\n")
+
+
+my_2nd_function()
+
